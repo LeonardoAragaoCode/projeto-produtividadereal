9998796a65688d4cb4a923dd2fe566c9c15ef6e7
leonardo aragao <85124953+LeonardoAragaoCode@users.noreply.github.com>
2022-03-08 21:25:08 -0300

    organizacao

--- temporizador.js
+++ temporizador.js
@@ -1,28 +1,43 @@
 (function ($) {
+    
+    let primeiroDigitoHora = 0
+    let segundoDigitoHora = 0
+    let primeiroDigitoMinuto = 0
+    let segundoDigitoMinuto = 0
+    let primeiroDigitoSegundo = 0
+    let segundoDigitoSegundo = 0
+    
+    
     $.fn.temporizador = function(parametros) {
         
-        const primeiroDigitoHora = $("<span>").html("0")
-        const segundoDigitoHora = $("<span>").html("0")
-        const primeiroDigitoMinuto = $("<span>").html("0")
-        const segundoDigitoMinuto = $("<span>").html("0")
-        const primeiroDigitoSegundo = $("<span>").html("0")
-        const segundoDigitoSegundo = $("<span>").html("0")
+        primeiroDigitoHora = $("<span>").html("0")
+        segundoDigitoHora = $("<span>").html("0")
+        primeiroDigitoMinuto = $("<span>").html("0")
+        segundoDigitoMinuto = $("<span>").html("0")
+        primeiroDigitoSegundo = $("<span>").html("0")
+        segundoDigitoSegundo = $("<span>").html("0")
 
         const separadorHoraMinuto = $("<span>").html(":")
         const separadorMinutoSegundo = $("<span>").html(":")
 
         $(this).append(primeiroDigitoHora, segundoDigitoHora, separadorHoraMinuto, primeiroDigitoMinuto, segundoDigitoMinuto, separadorMinutoSegundo, primeiroDigitoSegundo, segundoDigitoSegundo)
 
-        const regex = new RegExp(/(\d\d):(\d\d):(\d\d)/)
-        
-        let segundos = 50
-        let minutos = 59
-        let horas = 1
-        
-        
-        
-        let cronometro = setInterval(() => {
-        
+        return this
+    }
+
+    const regex = new RegExp(/(\d\d):(\d\d):(\d\d)/)
+    
+    let segundos = 0
+    let minutos = 0
+    let horas = 0
+
+    let cronometro
+    let tempoEncerrado
+    let horarioEmString
+
+    $.fn.iniciarCronometro = function() {
+        cronometro = setInterval(() => {
+
             segundos = segundos + 1
             
             if(segundos === 60) {
@@ -45,7 +60,7 @@
             let horasStringComZero = "0" + horasString
             let horasStringComZeroESlice = horasStringComZero.slice(-2)
 
-            let horarioEmString = `${horasStringComZeroESlice}:${minutosStringComZeroESlice}:${segundosStringComZeroESlice}`
+            horarioEmString = `${horasStringComZeroESlice}:${minutosStringComZeroESlice}:${segundosStringComZeroESlice}`
 
             let horarioRegex = regex.exec(horarioEmString)
 
@@ -57,7 +72,57 @@
             segundoDigitoSegundo.html(horarioRegex[3][1])
 
         }, 1000)
-    
         return this
     }
-}) (jQuery)
+
+    $.fn.pausarCronometro = function(){
+        clearInterval(cronometro)
+        return this
+    }
+
+    $.fn.encerrarCronometro = function (){
+        clearInterval(cronometro)
+        
+        tempoEncerrado = regex.exec(horarioEmString)
+        console.log(tempoEncerrado)
+
+        primeiroDigitoHora.html("0")
+        segundoDigitoHora.html("0")
+        primeiroDigitoMinuto.html("0")
+        segundoDigitoMinuto.html("0")
+        primeiroDigitoSegundo.html("0")
+        segundoDigitoSegundo.html("0")
+
+        segundos = 0
+        minutos = 0
+        horas = 0
+
+        return this
+    }
+
+    let materia 
+    let assunto
+    
+    $.fn.capturarTexto = function(){
+        materia = $('#campoMateria').val()
+        assunto = $('#campoAssunto').val()
+        console.log(materia)    
+        console.log(assunto)
+        return this
+    }
+
+    $.fn.registrador = function(){ 
+        let registroAtual = $('<div>').html(function() {
+            let agruparItens = materia + " - " + assunto + " - " + tempoEncerrado[0]
+            return "<p>" + agruparItens + "</p>"
+        })                                                                    
+        $(this).append(registroAtual)
+        return this
+    }
+
+    $.fn.limparCampos = function(){
+        $('#campoMateria').val("")
+        $('#campoAssunto').val("")
+        return this
+    }
+}) (jQuery) 
